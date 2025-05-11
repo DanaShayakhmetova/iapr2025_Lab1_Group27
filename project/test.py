ba349c2874b0fc524a3a8ec8bd3182a50c47c24d
zhua zhua <zhuoran.ren@epfl.ch>
2025-05-11 22:14:02 +0800

    change test.py

--- project/test.py
+++ project/test.py
@@ -3,7 +3,7 @@ from src.visulization import Visualization
 from src.utils import *
 
 find_mask = FindMask()
-image_path  = "data/data_project/test/L1000891.JPG"
+image_path  = "data/data_project/test/L1000942.JPG"
 image = read_image(image_path)
 hsv = find_mask.to_hsv(image)
 mask = find_mask.find_brown_mask(image)
